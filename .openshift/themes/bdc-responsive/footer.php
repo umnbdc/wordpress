a33edb726d6cacedaff9112ba96758c70b31ad9c
Kyle Condiff <kyle@Kyles-MacBook-Pro-9.local>
2016-05-21 22:34:02 -0500

    BDC Responsive: add some comments to the responsive CSS, make a new footer which removes the social icons and copyright, etc.

--- .openshift/themes/bdc-responsive/footer.php
+++ .openshift/themes/bdc-responsive/footer.php
@@ -41,38 +41,18 @@ $responsive_options = responsive_get_options();
 
 		<div class="grid col-940">
 
-			<div class="grid col-540">
-				<?php if ( has_nav_menu( 'footer-menu', 'responsive' ) ) {
-					wp_nav_menu( array(
-						'container'      => '',
-						'fallback_cb'    => false,
-						'menu_class'     => 'footer-menu',
-						'theme_location' => 'footer-menu'
-					) );
-				} ?>
-			</div><!-- end of col-540 -->
-
-			<div class="grid col-380 fit">
-				<?php echo responsive_get_social_icons() ?>
-			</div><!-- end of col-380 fit -->
+			<!-- Removed Social Icons and made the footer menu full-width -->
+			<?php if ( has_nav_menu( 'footer-menu', 'responsive' ) ) {
+				wp_nav_menu( array(
+					'container'      => '',
+					'fallback_cb'    => false,
+					'menu_class'     => 'footer-menu',
+					'theme_location' => 'footer-menu'
+				) );
+			} ?>
 
 		</div><!-- end of col-940 -->
 		<?php get_sidebar( 'colophon' ); ?>
-
-		<div class="grid col-300 copyright">
-			<?php esc_attr_e( '&copy;', 'responsive' ); ?> <?php echo date( 'Y' ); ?><a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-				<?php bloginfo( 'name' ); ?>
-			</a>
-		</div><!-- end of .copyright -->
-
-		<div class="grid col-300 scroll-top"><a href="#scroll-top" title="<?php esc_attr_e( 'scroll to top', 'responsive' ); ?>"><?php _e( '&uarr;', 'responsive' ); ?></a></div>
-
-		<div class="grid col-300 fit powered">
-			<a href="<?php echo esc_url( 'http://cyberchimps.com/responsive-theme/' ); ?>" title="<?php esc_attr_e( 'Responsive Theme', 'responsive' ); ?>">
-				Responsive Theme</a>
-			<?php esc_attr_e( 'powered by', 'responsive' ); ?> <a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>" title="<?php esc_attr_e( 'WordPress', 'responsive' ); ?>">
-				WordPress</a>
-		</div><!-- end .powered -->
 
 	</div><!-- end #footer-wrapper -->
 
